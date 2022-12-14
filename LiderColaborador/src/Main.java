473c211f919329eabd3608ea1bfa79032e64b87d
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2022-12-14 13:47:31 +0100

    Rama colaborador

--- LiderColaborador/src/Main.java
+++ LiderColaborador/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hey!");
+        System.out.println("Hola soy el colaborador Nico");
+
+        //colores
+        String color1, color2, color3;
+
+        color1 = "Verde";
+        color2 = "Amarillo";
+        color3 = "Violeta";
+
+        System.out.println(color1);
     }
 }
